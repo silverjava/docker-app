9574863f664a119b7d9d91e5d0076c704c61597b
silverjava <silverjava@gmail.com>
2014-10-09 22:36:00 +0800

    clean up the code

--- app/public/javascripts/directives.js
+++ app/public/javascripts/directives.js
@@ -4,11 +4,18 @@ angular.module('dockerApp.directives', [])
   .directive('term', ['$timeout', '$routeParams', 'socket', function($timeout, $routeParams, socket) {
     return {
       restrict: 'E',
-      template: '<div></div>',
+      template: '<span></span>',
       replace: true,
       link: function($scope, iElm, iAttrs, controller) {
-        var terminal = new Terminal();
+        var terminal = new Terminal({
+          cols: 80,
+          rows: 24,
+          useStyle: true,
+          screenKeys: true,
+          cursorBlink: false
+        });
         terminal.open();
+        terminal.write('\x1b[31mSession started !\x1b[m\r\n');
 
         var inputStream = ss.createStream();
 
@@ -24,6 +31,10 @@ angular.module('dockerApp.directives', [])
           stream.on('data', function(data) {
             terminal.write(data + "");
           })
+        });
+
+        $scope.$on('$destroy', function() {
+          terminal.destroy();
         });
       }
     };
